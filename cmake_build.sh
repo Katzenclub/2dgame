c945cc2c56d2e2a7c83fbbbbaec34e4bd2521d65
Chloe <thecamoc@gmail.com>
2022-01-31 13:12:37 +0100

    add the stupid libraries that were annoying but i did it thanks to bing.com

--- cmake_build.sh
+++ cmake_build.sh
@@ -1,11 +1,31 @@
+for var in "$@"
+do
+    if [ "$var" == "-g" ]
+    then
+        wget -O rapidxml.zip https://sourceforge.net/projects/rapidxml/files/latest/download
+        unzip -o rapidxml.zip -d libs
+        rm -f rapidxml.zip
+
+        wget -O imgui.zip https://github.com/ocornut/imgui/archive/refs/tags/v1.86.zip
+        unzip -o imgui.zip -d libs
+        rm -f imgui.zip
+
+        wget -O imgui-sfml.zip https://github.com/eliasdaler/imgui-sfml/archive/refs/tags/v2.5.zip
+        unzip -o imgui-sfml.zip -d libs
+        rm -f imgui-sfml.zip
+
+        cp -f libs/imgui-sfml-2.5/imconfig-SFML.h libs/imgui-1.86/imconfig.h
+    fi
+done
+
 mkdir Release
 cd Release
 cmake ../2DGame
 make
-if [ $# -gt 0 ]
-then
-    if [ $@ == "-s" ]
+for var in "$@"
+do
+    if [ "$var" == "-s" ]
     then
         ./2DGame
     fi
-fi
+done
